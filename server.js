97550971315f4501ce401afcf1a6ec066e5ce9bb
Sarusbr <batuilter2002@gmail.com>
2022-03-06 13:50:42 +0300

    kullanıcı ile giriş yapabilme sağlandı

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var fs = require("fs");
+const MongoClient = require('mongodb').MongoClient;
+const URL = 'mongodb+srv://testuser:testuserpassword@cluster0.5n3d2.mongodb.net/test';
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -17,12 +19,32 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/login",(req,res)=>{
-    if(req.body.username=="batu" && req.body.password =="123"){
-        res.send({"information":"başarılı"});
-        res.end();
-    }
-    else{
-        res.send({"information":"başarılı değil"});
-        res.end();
-    }
+    MongoClient.connect(URL,(err,client)=>{
+        if(err) throw err;
+        const db = client.db("forumdatabase");
+        db.collection("users").findOne({username:req.body.username,password:req.body.password},(err,data)=>{
+            if(data != null){
+                res.send({"information":"başarılı"});
+                res.end();
+            }
+            else{
+                res.send({"information":"başarısız"});
+                res.end();
+            }
+        })
+    })
+})
+
+app.get("/kullaniciadi",(req,res)=>{
+    MongoClient.connect(URL, (err, client) => {
+    if (err) throw err;
+    const db = client.db('forumdatabase');
+    db.collection('users').insertOne({ username:"batu" /*req.body.username*/, password: "123"/*req.body.password*/ }, (err, result) => {
+        if (err) throw err;
+            console.log('Başarılı bir şekilde eklendi.');
+            res.status(204).send();
+            res.end();
+            client.close();
+        })
+    })
 })
