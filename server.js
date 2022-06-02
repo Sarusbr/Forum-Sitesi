58237e856b03e573017004d0e910622bcf424ae2
Sarusbr <batuilter2002@gmail.com>
2022-06-02 20:10:42 +0300

    folder edit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
+
 app.listen(process.env.PORT || 80);
 
-app.use(express.static("../frontend/"))
+app.use(express.static("frontend/"))
 
 app.get("/",(req,res)=>{
-    fs.readFile("../frontend/forumTemplate.html",(err,data)=>{
+    fs.readFile("frontend/forumTemplate.html",(err,data)=>{
         res.write(data);
         res.end();
     })
