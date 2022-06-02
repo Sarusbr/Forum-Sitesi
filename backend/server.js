c5d006d9d8ab03fb27f2a29951b64329fec241c4
Sarusbr <batuilter2002@gmail.com>
2022-06-02 20:30:27 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ var fs = require("fs");
 
 app.listen(process.env.PORT || 80);
 
-app.use(express.static("frontend/"))
+app.use(express.static("../frontend/"))
 
 app.get("/",(req,res)=>{
-    fs.readFile("frontend/forumTemplate.html",(err,data)=>{
+    fs.readFile("../frontend/forumTemplate.html",(err,data)=>{
         res.write(data);
         res.end();
     })
